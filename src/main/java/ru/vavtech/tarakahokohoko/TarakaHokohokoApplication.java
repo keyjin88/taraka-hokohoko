5369d404faedb4d5a1272b55b743f168a28436f7
av <keyjin88@gmail.com>
2025-02-05 22:48:44 +0300

    Initial commit

---
+++ src/main/java/ru/vavtech/tarakahokohoko/TarakaHokohokoApplication.java
@@ -0,0 +1,13 @@
+package ru.vavtech.tarakahokohoko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarakaHokohokoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TarakaHokohokoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vavtech/tarakahokohoko/TarakaHokohokoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vavtech.tarakahokohoko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarakaHokohokoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
